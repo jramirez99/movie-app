0b14d6ed0ba71c82916d52f6f4a8b284d68796dd
jramirez99 <javierrk99@gmail.com>
2021-10-16 16:44:35 -0500

    created component

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Bar, NavBarContainer } from '../../styles/components/navbar/navBar';
+import { Nav } from './Nav';
+
+import logo from '../../images/logo.svg';
+
+export const NavBar = () => {
+    return (
+        <Bar>
+            <NavBarContainer className="container">
+                <Link to="/">
+                    <img 
+                        src={ logo }
+                        alt="logo"
+                    />
+                </Link>
+
+                <Nav />
+            </NavBarContainer>
+        </Bar>
+    );
+};
