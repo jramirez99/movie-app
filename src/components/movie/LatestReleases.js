579d362a260382b1010fd09e19020e1edef956bb
jramirez99 <javierrk99@gmail.com>
2021-10-16 13:06:19 -0500

    created components

---
+++ src/components/movie/LatestReleases.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const LatestReleases = () => {
+    return (
+        <div>
+            desde Latest releases
+        </div>
+    );
+};
---
+++ src/components/movie/Populars.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Populars = () => {
+    return (
+        <div>
+            desde populars
+        </div>
+    );
+};
