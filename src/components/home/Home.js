7d9c374e1140fe4c62b4a84e077cc84cffcf3165
jramirez99 <javierrk99@gmail.com>
2021-10-22 18:26:23 -0500

    add spinner

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,13 +3,16 @@ import React from 'react';
 import { useFetch } from '../../hooks/useFetch';
 import { baseURL, apiKey } from '../../config/api';
 import { SlidersMovies } from '../slidersMovies/SlidersMovies';
+import { Spinner } from '../spinner/Spinner';
 
 
 export const Home = () => {
+    const res = useFetch( `${ baseURL }/movie/now_playing?api_key=${ apiKey }&language=es-MX&page=1` );
+    if ( res.loading || !res.result ) {
+        return <Spinner />
+    };
 
-    const res = useFetch( `${ baseURL }/movie/popular?api_key=${ apiKey }&language=es-MX&page=1` );
     const movies =  res.result.results;
-    if( movies === undefined ) return null;
 
     return (
         <>
