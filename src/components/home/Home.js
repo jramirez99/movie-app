ab6f2c019d4173e94ec9a0a3f6058aaf0a174bdb
jramirez99 <javierrk99@gmail.com>
2021-10-22 15:19:53 -0500

    add global styles

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import { useFetch } from '../../hooks/useFetch';
+import { baseURL, apiKey } from '../../config/api';
+import { SlidersMovies } from '../slidersMovies/SlidersMovies';
+
+
 export const Home = () => {
+
+    const res = useFetch( `${ baseURL }/movie/popular?api_key=${ apiKey }&language=es-MX&page=1` );
+    const movies =  res.result.results;
+    if( movies === undefined ) return null;
+    // console.log(movies)
+    
     return (
-        <div>
-            desde Home
-        </div>
+        <>
+            <SlidersMovies 
+                movies={movies}
+            />
+
+        </>
     );
 };
