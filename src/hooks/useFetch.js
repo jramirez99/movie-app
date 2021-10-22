481301bc448f2d4ddf163c41584fc994ebc588ad
jramirez99 <javierrk99@gmail.com>
2021-10-22 18:27:04 -0500

    add states loading

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 
 export const useFetch = ( url ) => {
 
+    const [loading, setLoading] = useState( true )
     const [result, setResult] = useState( [] );
     
     useEffect( () => {
@@ -10,12 +11,14 @@ export const useFetch = ( url ) => {
                 const res = await fetch( url );
                 const data = await res.json();        
                 setResult( data );
+                setLoading( false );
             } catch (error) {
                 console.log(error);
+                setLoading( false );
             }
         };
         consularApi();
     }, [url]);
 
-    return { result };
+    return { result, loading };
 };
