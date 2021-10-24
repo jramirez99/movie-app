f2a8b7b60c9e5b7d75845b4c248abd528c641403
jramirez99 <javierrk99@gmail.com>
2021-10-24 15:18:05 -0500

    create component footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { P } from '../../styles/components/footer/footer';
+
+export const Footer = () => {
+    return (
+        <P>
+            Elabrodo por Javier Ramirez &copy;
+        </P>
+    );
+};
