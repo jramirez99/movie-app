5bcd0eb9885f9b27ff8a7284ba8730cb00ce7489
jramirez99 <javierrk99@gmail.com>
2021-10-27 18:20:18 -0500

    render the reusable component and send it as props the path to make the request

---
+++ src/components/movie/populars/Populars.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PaginationFetch } from '../../pagination/PaginationFetch';
+
+export const Populars = () => {
+    return (
+        <>
+            <PaginationFetch  
+                route="popular"
+            />
+        </>
+    );
+};
