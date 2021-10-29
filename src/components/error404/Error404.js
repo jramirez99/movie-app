b559f93164b123d43917ebff7730192b0eebdae9
jramirez99 <javierrk99@gmail.com>
2021-10-29 11:13:01 -0500

    import img and styles and create content for show

--- src/components/error404/Error404.js
+++ src/components/error404/Error404.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import img from '../../images/pageNotFound.svg';
+
+import { Container } from '../../styles/components/error303/error404';
+
 export const Error404 = () => {
     return (
-        <div>
-            desde Error
-        </div>
+        <Container>
+            <img 
+                src={ img }
+                alt="imagen"
+            />
+
+            <h2> Error página no encontrada </h2>
+        </Container>
     );
 };
